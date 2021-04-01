0379a195828ec8ad025a4f91ebecf6e3852a2e86
ABHISHEK Joshi <abhijoshi2k@gmail.com>
2021-04-01 18:33:43 +0530

    generate full matrix and bug fix

--- classifier.py
+++ classifier.py
@@ -58,27 +58,54 @@ def make_sparse_matrix(df, indexed_words):
 
     df: A DataFrame with words in the columns with a document id as an index (X_train or X_test)
     indexed_words: Index of words ordered by word id
-    labels: Category as a Series (y_train or y_test)
     """
 
-    nr_rows = df.shape[0]
     nr_cols = df.shape[1]
     word_set = set(indexed_words)
     dict_list = []
 
-    for i in range(nr_rows):
-        for j in range(nr_cols):
+    for j in range(nr_cols):
 
-            word = df.iat[i, j]
-            if word in word_set:
-                doc_id = df.index[i]
-                word_id = indexed_words.get_loc(word)
+        word = df.iat[0, j]
+        if word in word_set:
+            doc_id = df.index[0]
+            word_id = indexed_words.get_loc(word)
 
-                item = {'DOC_ID': doc_id, 'OCCURENCE': 1, 'WORD_ID': word_id}
+            item = {'DOC_ID': doc_id, 'OCCURENCE': 1, 'WORD_ID': word_id}
 
-                dict_list.append(item)
+            dict_list.append(item)
 
     return pd.DataFrame(dict_list)
+
+
+def make_full_matrix(sparse_matrix, nr_words, doc_idx=0, word_idx=1, freq_idx=2):
+    """
+    Form a full matrix full matrix from a sparse matrix. Return a pandas DataFrame.
+    Keywords arguments:
+    sparse_matrix -- numpy array
+    nr_words -- size of the vocabulary. Total number of tokens.
+    doc_idx -- position of the document id in sparse matrix. Default: 1st column
+    word_idx -- position of the word id in sparse matrix. Default: 2nd column
+    freq_idx -- position of occurrence of word in sparse matrix. Default 3rd column
+    """
+
+    column_names = ['DOC_ID'] + list(range(0, nr_words))
+    doc_id_names = np.unique(sparse_matrix[:, doc_idx])
+
+    full_matrix = pd.DataFrame(index=doc_id_names, columns=column_names)
+    full_matrix.fillna(value=0, inplace=True)
+
+    for i in range(sparse_matrix.shape[0]):
+        doc_nr = sparse_matrix[i][doc_idx]
+        word_id = sparse_matrix[i][word_idx]
+        occurrence = sparse_matrix[i][freq_idx]
+
+        full_matrix.at[doc_nr, 'DOC_ID'] = doc_nr
+        full_matrix.at[doc_nr, word_id] = occurrence
+
+    full_matrix.set_index('DOC_ID', inplace=True)
+
+    return full_matrix
 
 
 def is_spam(data, mode):
@@ -95,7 +122,10 @@ def is_spam(data, mode):
     sparse_matrix = make_sparse_matrix(word_columns_df, word_index).groupby([
         'DOC_ID', 'WORD_ID']).sum().reset_index()
 
-    print(sparse_matrix)
+    sparse_matrix = sparse_matrix.to_numpy()
+
+    full_matrix = make_full_matrix(sparse_matrix, vocab.shape[0])
+    print(full_matrix)
 
 
-is_spam('Hello boy', 2)
+is_spam('Hello hello boy http http', 2)
