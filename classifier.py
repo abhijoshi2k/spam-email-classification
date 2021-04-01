f8b1f817e3b1eb4122c5e257e1c8c1ac2eda35e5
ABHISHEK Joshi <abhijoshi2k@gmail.com>
2021-04-01 16:39:03 +0530

    Data preprocessing done

--- classifier.py
+++ classifier.py
@@ -7,10 +7,13 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 from bs4 import BeautifulSoup
+from pandas.core.arrays import sparse
 
 TOKEN_SPAM_PROB_FILE = 'SpamData/03_Testing/prob-spam.txt'
 TOKEN_HAM_PROB_FILE = 'SpamData/03_Testing/prob-nonspam.txt'
 TOKEN_ALL_PROB_FILE = 'SpamData/03_Testing/prob-all-tokens.txt'
+
+vocab = pd.read_csv('SpamData/01_Processing/word-by-id.csv')
 
 
 def get_email(data, mode):
@@ -49,8 +52,47 @@ def clean_message_no_html(message, stop_words):
     return filtered_words
 
 
+def make_sparse_matrix(df, indexed_words):
+    """
+    Returns Sparse Matrix as DataFrame.
+
+    df: A DataFrame with words in the columns with a document id as an index (X_train or X_test)
+    indexed_words: Index of words ordered by word id
+    labels: Category as a Series (y_train or y_test)
+    """
+
+    nr_rows = df.shape[0]
+    nr_cols = df.shape[1]
+    word_set = set(indexed_words)
+    dict_list = []
+
+    for i in range(nr_rows):
+        for j in range(nr_cols):
+
+            word = df.iat[i, j]
+            if word in word_set:
+                doc_id = df.index[i]
+                word_id = indexed_words.get_loc(word)
+
+                item = {'DOC_ID': doc_id, 'OCCURENCE': 1, 'WORD_ID': word_id}
+
+                dict_list.append(item)
+
+    return pd.DataFrame(dict_list)
+
+
 def is_spam(data, mode):
     message_body = get_email(data, mode)
 
-    email = clean_message_no_html(
+    clean_message = clean_message_no_html(
         message_body, stop_words=set(stopwords.words('english')))
+
+    word_columns_df = pd.DataFrame.from_records([clean_message])
+    word_columns_df.index.name = 'DOC_ID'
+
+    word_index = pd.Index(vocab.VOCAB_WORD)
+
+    sparse_matrix = make_sparse_matrix(word_columns_df, word_index).groupby([
+        'DOC_ID', 'WORD_ID']).sum().reset_index()
+
+    print(sparse_matrix)
